88b80070b0e262fd01adc15679993d97b98911d9
mallano <maelallano1@gmail.com>
2018-12-11 11:14:35 +0100

    form done

--- client/form.js
+++ client/form.js
@@ -1,12 +1,11 @@
-// import { Template } from "meteor/templating";
-// import playlist from "../imports/db/playlist";
+import { Template } from "meteor/templating";
+import peopleDB from "../imports/db/peopleDB";
 
-// Template.form.events({
-//     "click #addSong": (event, template) => {
-//         // console.log(template.find("#song").value);
-//         playlist.insert({
-//             song: template.find("#song").value,
-//             artist: template.find("#artist").value,
-//         })
-//     }
-// })
+Template.form.events({
+    "click #addGuyBtn": (event, template) => {
+        peopleDB.insert({
+            name: template.find("#name").value,
+            mail: template.find("#mail").value,
+        })
+    }
+})
