01273aef1995d02dd7f00de5bda30afd91d10c04
mallano <maelallano1@gmail.com>
2019-01-14 09:16:05 +0100

    jsp

--- client/main.js
+++ client/main.js
@@ -1,5 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
 import './form.html';
 import './form.js';
 import './hello.html';
 import './peopleDB';
 import './main.html';
+
+ReactDOM.render(<App />, document.getElementById('root'));
