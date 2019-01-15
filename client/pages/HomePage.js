d6770962cfda38c4e16f8e00e854f3baa01d6124
mallano <maelallano1@gmail.com>
2019-01-15 11:40:25 +0100

    update done

--- client/pages/HomePage.js
+++ client/pages/HomePage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import peopleDB from '../../imports/db/peopleDB';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Link } from "react-router-dom";
+import Form from '../components/Form/Form';
 
 const HomePage = ({ tasks }) => (
     <div>
         <h1>WELCOME TO THE HOMEPAGE GUYS</h1>
+        <Form />
         <ul>
             {tasks.map(p => (
                 <li key={p._id}>Name: { p.name }, mail: { p.mail } <Link to={`/single/${p._id}`}>Click here please</Link></li>
--- client/pages/SinglePage.js
+++ client/pages/SinglePage.js
@@ -7,6 +7,7 @@ class SinglePage extends Component {
     state = {
         inputName: '',
         inputMail: '',
+        isEdit: false,
     }
 
     handleClick = e => {
@@ -14,20 +15,42 @@ class SinglePage extends Component {
         window.location.replace('/');
     }
 
-    // nameInputChange = e => {
-    //     console.log(e.target)
-    //     this.setState({
-    //         nameInputValue: e.target.getAttribute('value'),
-    //     })
-    // }
+    editValues = e => {
+        if (this.state.isEdit) {
+            peopleDB.update(e, {
+                $set: {
+                        name: this.state.inputName,
+                        mail: this.state.inputMail
+                    }
+            })
+        }
+        this.setState({
+            isEdit: !this.state.isEdit,
+        })
+    }
 
-    // mailInputChange = () => {
-    //     console.log('oui')
-    // }
+    nameInputChange = e => {
+        this.setState({
+            inputName: e.target.value,
+        })
+    }
+
+    mailInputChange = e => {
+        this.setState({
+            inputMail: e.target.value,
+        })
+    }
 
     render() {
         const user = this.props.tasks.filter(task => task._id === this.props.match.params.id)[0];
-        const displayed = user ? <p key={user._id}>Name: { user.name }, mail: { user.mail } <button onClick={() => this.handleClick(user._id)}>REMOVE</button></p> : <span>LOADING</span>
+        const displayed = user ? 
+        <p key={user._id}>
+            Name: { this.state.isEdit ? <input onChange={this.nameInputChange} value={this.state.inputName}/> : this.state.inputName ? this.state.inputName : user.name }, 
+            Mail: { this.state.isEdit ? <input onChange={this.mailInputChange} value={this.state.inputMail}/> : this.state.inputMail ? this.state.inputMail : user.mail }, 
+            <button onClick={() => this.editValues(user._id)}>EDIT</button> 
+            <button onClick={() => this.handleClick(user._id)}>REMOVE</button>
+        </p> : <span>LOADING</span>
+
         return (
             <div name="hello">
                 <Link to='/'>Back home</Link>
